5f4e1d1dd2f665962c25bedfedb8e0d1608e8d59
natthanan135 <natthanan.sa.65@ubu.ac.th>
2023-06-20 12:07:30 +0700

    create react app

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return(
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Natthanan semruamg</span>
+        </div>
+    );
+}
+
+export default Footer;
