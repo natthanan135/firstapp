bb833e46f8ab09ce981189bd0e73bdf47190c07f
natthanan135 <natthanan.sa.65@ubu.ac.th>
2023-06-27 13:33:23 +0700

    app1

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import Footer from './Footer';
 import { useState } from 'react';
 
 function App() {
-  const [num,setNum] = useState(0);
+  const [num, setNum] = useState(0);
+  const [Total, setTotal] = useState('');
+
+  const PrimeNumCheck = (number) => {
+    let result = '';
+    if (number % 2 === 0) {
+      result = `${number} <b>ไม่</b>เป็นจำนวนเฉพาะ`;
+    } else {
+      result = `${number} เป็นจำนวนเฉพาะ`;
+    }
+    setTotal(result);
+  };
+
+  const Add = () => {
+    setNum(num + 1);
+    PrimeNumCheck(num + 1);
+  };
+
+  const Delete = () => {
+    setNum(num - 1);
+    PrimeNumCheck(num - 1);
+  };
+
+  
   return (
     <div className="App">
-      <Header/>
-      <div className='MyContent' >
-        <h2>{num}</h2>
-        <button onClick={()=>setNum(num + 1)}>เพิ่ม</button>
-        <button onClick={()=>setNum(num - 1)}>ลด</button>
+      <Header />
+      <div className="MyContent">
+      <h1>{num}</h1><br></br>
+      <button className="btn-default" onClick={Add}>เพิ่ม</button>{'  '}
+      <button className="btn-secon" onClick={Delete}>ลด</button>{'  '}
+      <br></br><br></br>
+      <span dangerouslySetInnerHTML={{ __html: Total }}></span>
       </div>
-      <Footer/>
+    <Footer />
     </div>
   );
 }
